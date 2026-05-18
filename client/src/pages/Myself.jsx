8ef8e0e79ba9b9ab3ee76280e3844bb326f23cf0
ricaym <aymerincchassagne@gmail.com>
2026-05-18 23:22:44 +0200

    NEW images + Update Myself page

--- client/src/pages/Myself.jsx
+++ client/src/pages/Myself.jsx
@@ -2,10 +2,10 @@ import "../index.css"
 
 export default function Myself() {
 	return (
-		<div className="text-white flex flex-col items-center justify-center h-[80vh] text-center">
-			<legend>Salut, moi c’est Aymeric Chassagne</legend>
-
-			<p>Développeur web passionné, je crée des applications modernes avec React et Node.js.</p>
+		<div className="myselfCard">
+			<img className="profilePicture" src="../../public/profile_picture.png" />
+			<legend>Aymeric Chassagne</legend>
+			<p>Développeur Web App.</p>
 		</div>
 	);
 }
