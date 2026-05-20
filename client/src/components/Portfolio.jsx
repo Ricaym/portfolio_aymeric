27550e848d3c4921ebe7df655f05a78a1de67737
ricaym <aymerincchassagne@gmail.com>
2026-05-20 16:14:03 +0200

    Update Portfolio.jsx & CSS

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -1,19 +1,28 @@
-import "../index.css"
+import "../index.css";
 
 export default function Portfolio() {
+
 	return (
 		<div className="page">
 			<legend>Aymeric Chassagne</legend>
+
 			<div className="myselfCard">
 				<div className="profilePicture">
-					<p>Aymeric Chassagne</p>
-					<p>Développeur Web App.</p>
+					<p className="name_card">Aymeric Chassagne</p>
+					<p className="title_card">Web App Developer</p>
+				</div>
+				<div className="description">
+					<p className="description_paragraph">
+						Hello, my name is Aymeric Chasssagne.<br/><br/>
+						I am a web developer for Conduent Business Solutions France.<br/>
+					</p>
 				</div>
 			</div>
-			<div className="profilePicture_phone">
+
+			{/* <div className="profilePicture_phone">
 				<p className="phone">Aymeric Chassagne</p>
 				<p className="phone">Développeur Web App.</p>
-			</div>
+			</div> */}
 		</div>
 	);
 }
