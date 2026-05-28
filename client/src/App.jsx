f5fd4a6f58f25354daf5b71347a392e3ad7f2691
ricaym <aymerincchassagne@gmail.com>
2026-05-28 19:23:40 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,31 +2,53 @@ import { useState } from "react";
 import StartScreen from "./components/StartScreen";
 import CharacterSelection from "./components/CharacterSelection";
 import Portfolio from "./components/Portfolio";
-
+import LoadingScreen from "./components/LoadingScreen";
+// import { useGamepadNavigation } from "../hooks/useGamepadNavigation";
+// import { GamepadCursor } from "../hooks/GamepadCursor";
+import Cursor from "../hooks/Cursor";
 
 function App() {
 	const [step, setStep] = useState("start");
 	const [selectedCharacter, setSelectedCharacter] = useState(null);
-	const handleStart = () => {
+	const [nextStep, setNextStep] = useState(null);
 
-		setStep("selection");
-	};
-	const handleCharacterSelect = (character) => {
-		setSelectedCharacter(character);
+	// --- CURSEUR GLOBAL
+	// const { cursor } = useGamepadNavigation();
+
+	const showLoadingThen = (destination) => {
+		setNextStep(destination);
+		setStep("loading");
 
 		setTimeout(() => {
-			setStep("portfolio");
-		}, 250);
+			setStep(destination);
+		}, 2000);
+	};
+
+	const handleStart = () => {
+		showLoadingThen("selection");
+	};
+
+	const handleCharacterSelect = (character) => {
+		setSelectedCharacter(character);
+		showLoadingThen("portfolio");
 	};
 
 	return (
 		<>
+			{/* CURSEUR GLOBAL (toutes les pages) */}
+			<Cursor/>
+			{/* <GamepadCursor x={cursor.x} y={cursor.y} /> */}
+
 			{step === "start" && (
 				<StartScreen onStart={handleStart} />
 			)}
 
+			{step === "loading" && (
+				<LoadingScreen nextStep={nextStep} />
+			)}
+
 			{step === "selection" && (
-				<CharacterSelection onSelect={handleCharacterSelect}/>
+				<CharacterSelection onSelect={handleCharacterSelect} />
 			)}
 
 			{step === "portfolio" && (
