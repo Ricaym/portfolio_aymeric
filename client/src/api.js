ac9bbf75e554eafeaab2ce0f343022ecc42d403f
ricaym <aymerincchassagne@gmail.com>
2026-07-07 13:07:58 +0200

    Reset project to NEW portfolio

--- client/src/api.js
+++
@@ -1,6 +0,0 @@
-const API_URL = "http://localhost:5000";
-
-export const getProjects = async () => {
-  const res = await fetch(`${API_URL}/projects`);
-  return res.json();
-};
--- client/src/components/BackButton.jsx
+++
@@ -1,9 +0,0 @@
-function BackButton({ onBack }) {
-	return (
-		<button type="button" onClick={onBack} className="BackButton">
-			← Retour
-		</button>
-	);
-}
-
-export default BackButton;
