7fb41ece7981cd9701486f35d61cb3078ece8fab
Aymeric <aymeric.chassagne@conduent.com>
2026-07-24 10:02:21 +0200

    Update Gamemode page

--- client/src/components/Gamemode.jsx
+++ client/src/components/Gamemode.jsx
@@ -1,25 +1,11 @@
 function Gamemode({ onSelectMode }) {
 	return (
 		<div className="Screen">
-			{/* <div className="SelectModeRules">
-				<p>
-					Ceci est un portfolio récréatif, et a pour but de montrer l'ensemble de mes compétences liées au développement. <br />
-					Découvrez cet univers inspiré de Destiny 2. <br />
-					N'oubliez pas de faire F11 pour vous mettre en plein écran et profitez pleinement de votre expérience. <br />
-					L'entiéreté de ce site à été produit et imaginé par Aymeric Chassagne (moi-même) exceptés les images. <br />
-					Cette exprérience propose également une immersion à la manette. <br />
-					Surtout n'oubliez pas de tout tester et de me dire ce que vous en avez pensé. <br />
-					Merci à vous, bonne visite. <br />
-				</p>
-			</div> */}
 
 			<div className="SelectMode">
-				<p className="SelectModeTitle">
-					Choisis ton mode
-				</p>
 				<div className="SelectModeButtons">
 					<button onClick={() => onSelectMode("mouse")}>
-						Mode Souris
+						Mouse
 					</button>
 					<button
 						className="controllerMode"
@@ -29,7 +15,7 @@ function Gamemode({ onSelectMode }) {
 						}}
 						onClick={() => onSelectMode("gamepad")}
 					>
-						Mode Manette
+						Controller
 					</button>
 				</div>
 			</div>
