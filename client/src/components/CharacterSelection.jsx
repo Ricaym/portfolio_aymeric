da1c04042260d2f78c97a98708e01c218efc5003
Aymeric <aymeric.chassagne@conduent.com>
2026-07-24 10:02:07 +0200

    Update CharacterSelection page

--- client/src/components/CharacterSelection.jsx
+++ client/src/components/CharacterSelection.jsx
@@ -1,22 +1,22 @@
 import Companion from "./Companion";
-import First from "/public/1.jpg";
-import Second from "/public/2.jpg";
-import Third from "/public/3.jpg";
+import First from "/public/1.png";
+import Second from "/public/2.png";
+import Third from "/public/3.png";
 
 export default function CharacterSelection({ onSelect }) {
 	const characters = [
 		{
-			name: "A propos de moi !",
+			name: "About me !",
 			image: First,
 			destination: "myself",
 		},
 		{
-			name: "Mes Projets",
+			name: "My projects",
 			image: Second,
 			destination: "projects",
 		},
 		{
-			name: "Mes Contacts",
+			name: "My Contacts",
 			image: Third,
 			destination: "contacts",
 		},
@@ -31,6 +31,7 @@ export default function CharacterSelection({ onSelect }) {
 					<div key={character.name} className="CharacterCard" onClick={() => onSelect(character)}>
 						<p className="CharacterName">{character.name}</p>
 						<img className="CharacterImage" src={character.image} alt={character.name} />
+						<i class="fi fi-br-6"></i>
 					</div>
 				))}
 			</div>
